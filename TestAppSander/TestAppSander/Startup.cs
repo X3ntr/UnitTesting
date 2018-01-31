a6238d9d7f55a522e49a1588c4fd1ad6a1e9cb36
X3ntr _ <x3ntr.x3ntr@gmail.com>
2018-01-31 15:26:18 +0100

    test app sander added

---
+++ TestAppSander/TestAppSander/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestAppSander.Startup))]
+namespace TestAppSander
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
