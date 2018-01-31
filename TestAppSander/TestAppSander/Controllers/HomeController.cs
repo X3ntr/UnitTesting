775c57d4814409546990829b8535a2dad87d3df6
X3ntr _ <x3ntr.x3ntr@gmail.com>
2018-01-31 16:13:42 +0100

    updated test project sander

--- TestAppSander/TestAppSander/Controllers/HomeController.cs
+++ TestAppSander/TestAppSander/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace TestAppSander.Controllers
 {
+    [RequireHttps] //forces use of SSL
     public class HomeController : Controller
     {
         public ActionResult Index()
