e49886c44ec3596799ae7f9b26fbe4b822f5f998
ChristopheD2 <christophe.dumont5@hotmail.com>
2018-08-02 14:40:02 +0200

    TicketControllerTest created

---
+++ UnitTesting/Unit Testing Project/Eg_SupportCenter/UI-MVC.UnitTests/TicketControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SC.UI.Web.MVC.Controllers;
+using System.Web.Mvc;
+using Moq;
+using SC.BL.Domain;
+
+namespace UI_MVC.UnitTests
+{
+    [TestClass]
+    public class TicketControllerTests
+    {
+        [TestMethod]
+        public void TestDetailsView()
+        {
+            //Ticket ticket = new Mock(ticket); // Need a mock ticket, WIP
+            //var controller = new TicketController(); // the Controller to test
+            //var result = controller.Details(2) as ViewResult; // A random ticket ID
+            //
+            //
+            //Assert.AreEqual(, result.ViewName); // Requires a ticket to return
+        }
+    }
+}
