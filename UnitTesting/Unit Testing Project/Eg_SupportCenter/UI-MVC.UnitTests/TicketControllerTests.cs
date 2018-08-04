a3541750f14f164cab74d8761cfc99e7b57aa348
ChristopheD2 <christophe.dumont5@hotmail.com>
2018-08-04 12:26:18 +0200

    minor namechange to TicketControllerTests.cs

--- UnitTesting/Unit Testing Project/Eg_SupportCenter/UI-MVC.UnitTests/TicketControllerTests.cs
+++ UnitTesting/Unit Testing Project/Eg_SupportCenter/UI-MVC.UnitTests/TicketControllerTests.cs
@@ -3,6 +3,7 @@ using SC.UI.Web.MVC.Controllers;
 using System.Web.Mvc;
 using Moq;
 using SC.BL.Domain;
+using SC.BL;
 using System.Linq;
 using System.Linq.Expressions;
 
@@ -11,18 +12,22 @@ namespace UI_MVC.UnitTests
     [TestClass]
     public class TicketControllerTests
     {
+        //global data sets to use when mocking
+        //private static ITicketManager mgr = new TicketManager();
+
         [TestMethod]
-        public void TestDetailsView()
+        public void Details_ShowDetails_ReturnsDetailsView()
         {
+            //problem with system.web.mvc / a dependency file not found error BUG
             //Arrange
-            Mock<Ticket> ticket = new Mock<Ticket>(); // Need a mock ticket, WIP
+            //Mock<Ticket> ticket = new Mock<Ticket>(); // Need a mock ticket
             var controller = new TicketController(); // the Controller to test
 
             //Act
-            var result = controller.Details(2) as ViewResult; // A random ticket ID
+            var result = controller.Details(2) as ViewResult; 
 
             //Assert
-            Assert.AreEqual(ticket, result.ViewName); // Requires a ticket to return
+            Assert.AreEqual("Details", result.ViewName); // Returns view name
         }
     }
 }
